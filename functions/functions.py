a57d0c2f447da9382765893f2b45fa7d3b3b95b8
cschaffner <huebli@gmail.com>
2016-11-17 13:55:50 +0100

    final version

--- functions/functions.py
+++ functions/functions.py
@@ -17,10 +17,10 @@ import random
 
 #2 Recall that the factorial of a positive integer x is defined as x! = x(x-1)!. The factorial of 0 is 0! = 1.
 # Implement the function factorial that takes non-negative integers as arguments. If the input is not an integer
-# or negative, the function should print "Error: Invalid input <input value> to factorial()". Otherwise it should 
+# or negative, the function should return "Error: Invalid input <input value> to factorial()". Otherwise it should 
 # return the value of the factorial function for that input. Once you have  written the factorial function, use it to 
 # implement the binom_coefficient function which computes the binomial coefficient. If the second argument to that 
-# function is greater than the first one, the function prints 
+# function is greater than the first one, the function return 
 # "Error: second argument <value of second argument> of binom_coefficient is bigger than the first one <value of first argument>".
 # Notice that by calling factorial inside binomial_coefficient you have already guarded against invalid input in the
 # form of non-integers or negative numbers.
@@ -72,33 +72,14 @@ print((lambda x : x**2) (3))
 # - Return the first letter of a string (that is the first non-whitespace character)
 # - Check whether the number 1024 is in a list of numbers
 # - Transform a number into a string
-# - Takes two arguments and add them up (you cannot put the args in tuple or list because then you'd reduce them to only one arg)
+# - Takes two arguments and add them up 
 # - Takes two arguments and checks whether they are of the same type
 
 alice = " Alice"
 numbers1 = list(range(1010, 1025))
 numbers2 = list(range(100,200))
 
-first_char = alice == " "
-first_letter = alice == "A"
-number_1024_in_list = False
-number_1024_not_in_list = False
-transform_number = 2 == "2"
-add_up = False
-type_check_true = False
-type_check_false = False
-
-print("First char works: {}".format(first_char))
-print("First letter works: {}".format(first_letter))
-print("Correct if 1024 in list: {}".format(number_1024_in_list))
-print("Correct if 1024 not in list: {}".format(number_1024_not_in_list))
-print("Number transformation works: {}".format(transform_number))
-print("Addition works: {}".format(add_up))
-print("Type check if types match works: {}".format(type_check_true))
-print("Type check if types don't match works: {}".format(type_check_false))
-
-
-first_char = (lambda x: )(alice) 
+first_char = (lambda )(alice) 
 first_letter = (lambda )(alice)
 number_1024_in_list = (lambda )(numbers1)
 number_1024_not_in_list = (lambda )(numbers2)
@@ -113,5 +94,5 @@ print("Correct if 1024 in list: {}".format(number_1024_in_list == False))
 print("Correct if 1024 not in list: {}".format(number_1024_not_in_list == False))
 print("Number transformation works: {}".format(transform_number == "2"))
 print("Addition works: {}".format(add_up == 57))
-print("Type check if types match works: {}".format(type_check_true = True))
-print("Type check if types don't match works: {}".format(type_check_false = False))
+print("Type check if types match works: {}".format(type_check_true == True))
+print("Type check if types don't match works: {}".format(type_check_false == False))
